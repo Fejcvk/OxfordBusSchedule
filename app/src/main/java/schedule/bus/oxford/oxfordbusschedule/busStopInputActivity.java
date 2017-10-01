1fa703ae7eb27db9ff49da66820f9d0bcbbe2048
Fejcvk <tomaszfejcak@icloud.com>
2017-10-01 18:35:31 +0100

    list view added

--- app/src/main/java/schedule/bus/oxford/oxfordbusschedule/busStopInputActivity.java
+++ app/src/main/java/schedule/bus/oxford/oxfordbusschedule/busStopInputActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.MultiAutoCompleteTextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Tomasz on 01/10/2017.
@@ -16,7 +19,8 @@ public class busStopInputActivity extends AppCompatActivity {
     BusstopManager busstopmanager;
     String[] autocomplete;
     AutoCompleteTextView autoCompleteTextView;
-
+    ListView listView;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,9 +28,30 @@ public class busStopInputActivity extends AppCompatActivity {
         autocomplete = busstopmanager.getBusStopNames();
         setContentView(R.layout.input_stop_activity);
         autoCompleteTextView = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
-        ArrayAdapter adapter = new
+        adapter = new
                 ArrayAdapter(this,android.R.layout.simple_list_item_1,autocomplete);
         autoCompleteTextView.setAdapter(adapter);
         autoCompleteTextView.setThreshold(1);
+        listView = (ListView)findViewById(R.id.busListView);
+        listViewFillUp(listView);
+    }
+    void listViewFillUp(ListView lV)
+    {
+        List list = new ArrayList();
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        list.add("KUTAS");
+        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        lV.setAdapter(adapter);
     }
 }
